c3cce2b4d78935a90aab8767c1c5efd17c213c15
kodebykush <Kodewithkush@gmail.com>
2025-02-26 09:51:36 +0530

    Initial commit: Weather App with dynamic backgrounds and temperature conversion

--- config.example.js
+++
@@ -1,7 +0,0 @@
-// Example configuration file
-// Rename this file to config.js and add your OpenWeatherMap API key
-const config = {
-    apiKey: 'YOUR_API_KEY_HERE'
-};
-
-export default config;
--- config.js
+++
@@ -1,6 +0,0 @@
-// Store your API key here and add this file to .gitignore
-const config = {
-    apiKey: '90d51a48124d37d90a88f806318e3c55'
-};
-
-export default config;
